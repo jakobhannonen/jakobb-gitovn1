c4449becf06030385d2f3e060580d91a852f61d5
jakobhannonen <48241210+jakobhannonen@users.noreply.github.com>
2019-03-06 13:13:39 +0200

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,9 +1,14 @@
 //slumptal mellan 1 och 39
 //OBS dålig kod, mjlighet till dublikat => använd inte ;)
 function lottoRad() {
+	alert("här är dina lottonumror");
 	for (var i=0; i<=7; i++) {
 
 		let lottery = Math.floor(Math.random() * 38)+1;
 	 	document.getElementById ("lotto_" + i).innerHTML = lottery;
+		
+		
+		
 	}
 }
+
